e1b9285c15ec43533d270d430844e975dc09a349
Compulink-Dev <devcompulink@gmail.com>
2024-08-08 16:37:58 +0200

    main

---
+++ app/api/products/route.ts
@@ -0,0 +1,10 @@
+import { auth } from '@/lib/auth'
+import dbConnect from '@/lib/dbConnect'
+import ProductModel from '@/lib/models/ProductModel'
+
+export const GET = (async (req: any) => {
+    await dbConnect()
+    const products = await ProductModel.find()
+    return Response.json(products)
+}) as any
+
