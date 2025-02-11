060d2bb862997b06e6a01ee0de3bac3d37077206
Compulink-Dev <devcompulink@gmail.com>
2025-02-11 14:59:26 +0200

    main

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,14 +1,40 @@
 import dbConnect from "@/lib/dbConnect";
 import OrderModel from "@/lib/models/OrderModel";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+
+// const validStatuses = [
+//   "Order Received",
+//   "Shipped",
+//   "In Transit",
+//   "Out for Delivery",
+//   "Delivered",
+//   "Collected",
+// ];
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   await dbConnect();
-  const order = await OrderModel.findById(params.id);
-  return NextResponse.json(order);
+
+  try {
+    const { id } = params;
+    const order = await OrderModel.findById(id)
+      .populate({ path: "tracking.product", select: "name slug" })
+      .populate({ path: "user", select: "name email" });
+
+    if (!order) {
+      return NextResponse.json({ message: "Order not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ tracking: order.tracking }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching tracking data:", error);
+    return NextResponse.json(
+      { message: "An unknown error occurred" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function PATCH(
