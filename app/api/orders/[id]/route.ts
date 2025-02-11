842f375a3a6811dbae3b7a6778376bb314c8db6d
Compulink-Dev <devcompulink@gmail.com>
2025-02-11 15:04:05 +0200

    main

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,62 +1,62 @@
 import dbConnect from "@/lib/dbConnect";
 import OrderModel from "@/lib/models/OrderModel";
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// const validStatuses = [
-//   "Order Received",
-//   "Shipped",
-//   "In Transit",
-//   "Out for Delivery",
-//   "Delivered",
-//   "Collected",
-// ];
-
+// GET Order by ID
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   await dbConnect();
-
   try {
-    const { id } = params;
-    const order = await OrderModel.findById(id)
-      .populate({ path: "tracking.product", select: "name slug" })
-      .populate({ path: "user", select: "name email" });
+    const order = await OrderModel.findById(params.id);
 
     if (!order) {
       return NextResponse.json({ message: "Order not found" }, { status: 404 });
     }
 
-    return NextResponse.json({ tracking: order.tracking }, { status: 200 });
+    return NextResponse.json(order);
   } catch (error) {
-    console.error("Error fetching tracking data:", error);
+    console.error("Error fetching order:", error);
     return NextResponse.json(
-      { message: "An unknown error occurred" },
+      { message: "Error fetching order", error },
       { status: 500 }
     );
   }
 }
 
+// PATCH - Update Order Payment Status
 export async function PATCH(
-  req: Request,
-  { params }: { params: { orderId: string } }
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ) {
   try {
     await dbConnect();
-    const { orderId } = params;
-    const { isPaid } = await req.json(); // Assuming payment update sends { isPaid: true }
+    const { id } = params;
+    const { isPaid } = await req.json();
 
+    // Validate isPaid input
+    if (typeof isPaid !== "boolean") {
+      return NextResponse.json(
+        { message: "Invalid value for isPaid. Must be a boolean." },
+        { status: 400 }
+      );
+    }
+
+    // Update order
     const updatedOrder = await OrderModel.findByIdAndUpdate(
-      orderId,
+      id,
       { isPaid },
       { new: true }
     );
 
-    if (!updatedOrder)
+    if (!updatedOrder) {
       return NextResponse.json({ message: "Order not found" }, { status: 404 });
+    }
 
     return NextResponse.json(updatedOrder);
   } catch (error) {
+    console.error("Error updating order:", error);
     return NextResponse.json(
       { message: "Error updating order", error },
       { status: 500 }
