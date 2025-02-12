fdad189ac90a67befa5e560f793e6979002cf459
Compulink-Dev <devcompulink@gmail.com>
2025-02-12 10:36:50 +0200

    main

--- lib/services/bannerServices.ts
+++ lib/services/bannerServices.ts
@@ -1,12 +1,11 @@
-import { cache } from "react"
-import dbConnect from "../dbConnect"
-import BannerModel, { Banner } from "../models/BannerModel"
-
+import { cache } from "react";
+import dbConnect from "../dbConnect";
+import BannerModel from "../models/BannerModel";
 
 const getBanners = cache(async () => {
-    await dbConnect()
-    const banner = await BannerModel.find().limit(3).lean()
-    return banner as Banner[]
-})
+  await dbConnect();
+  const banner = await BannerModel.find().limit(3).lean();
+  return banner as any[];
+});
 
-export default getBanners
+export default getBanners;
